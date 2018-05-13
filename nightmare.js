baa2c45d34d29da7eff8f9daedb78887f2aed06f
Sumod Kgit config --global user.name Sumod <>
2018-05-13 21:33:46 +0900

    first commit

--- nightmare.js
+++ nightmare.js
@@ -9,8 +9,6 @@ var email = login.getEmail();
 var password = login.getPassword();
 
 var beginNightmare = function(url){
-	console.log('email = ', email)
-	console.log('password = ', password)
 	nightmare
 	.useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
 	.goto(url)
